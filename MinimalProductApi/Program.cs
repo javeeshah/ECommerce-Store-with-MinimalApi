61ab66254924ad86c895bc4c69d6f309b4b80f1a
Jawad Hussain <javee_shah@hotmail.com>
2026-03-02 01:01:54 +1100

    Removed unnessary using statements

--- MinimalProductApi/Program.cs
+++ MinimalProductApi/Program.cs
@@ -1,13 +1,10 @@
 using MinimalProductApi.DbContexts;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
 using MinimalProductApi.Dtos;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
